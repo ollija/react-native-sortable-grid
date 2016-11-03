4e8daf571628f91f821b54915b7996f7b70e8f19
Olli Jaakkola <olli.jaakkola@leonidasoy.fi>
2016-11-03 05:54:39 +0200

    Fix deletionExample for android

--- example/deletionExample.js
+++ example/deletionExample.js
@@ -12,7 +12,7 @@ export default class basicExample extends Component {
 
   constructor() {
     super()
-    this.numbers = [...Array(12).keys()]
+    this.numbers = [0,1,2,3,4,5,6,7,8,9,10,11]
   }
 
   getColor() {
